9dab034c1a04f93db29380b8615353b9e88edfba
mmorey05 <mmorey@udel.edu>
2026-04-08 14:40:35 -0400

    starting Part C

--- hw-4-/Interactable's.gd
+++ hw-4-/Interactable's.gd
@@ -1,11 +1,8 @@
-extends Area2D
+class_name Interactable extends Area2D
 
+@export var interaction_name = "test interaction"
+@export var interaction_type = "test"
+@export var display_active = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func interact():
+	push_warning("This Interactable has no interact() code yet!")
