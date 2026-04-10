b8f1a08fce9b19c11f01f058e22a075993576567
esaylor1275 <esaylor1275@gmail.com>
2026-04-10 14:08:27 -0400

    Update player.gd
    Player can now move

--- hw-4-/player.gd
+++ hw-4-/player.gd
@@ -2,24 +2,35 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 
-var nearby_interactables = null
+var nearby_interactables = []
 
-func _physics_process(_delta: float) -> void:
-	var input_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_vector * speed
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+
+	if Input.is_key_pressed(KEY_A):
+		direction.x -= 1
+	if Input.is_key_pressed(KEY_D):
+		direction.x += 1
+	if Input.is_key_pressed(KEY_W):
+		direction.y -= 1
+	if Input.is_key_pressed(KEY_S):
+		direction.y += 1
+
+	velocity = direction * speed
 	move_and_slide()
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("interact"):
-		if nearby_interactables:
-				nearby_interactables.back().interact()
+		if nearby_interactables.size() > 0:
+			nearby_interactables.back().interact()
+
 
 func _on_interaction_area_area_entered(area: Area2D) -> void:
 	print("interactable detected")
-	area.set_active(true)
 	nearby_interactables.append(area)
 
+
 func _on_interaction_area_area_exited(area: Area2D) -> void:
-	print("interactable detected")
-	area.set_active(false)
+	print("interactable exited")
 	nearby_interactables.erase(area)
